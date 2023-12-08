7441c3ba1642e445bb321d8f9b33a41c30ff6437
Abhinav Srivastava <abhinav.srivstva@gmail.com>
2023-12-08 14:14:59 +0530

    added actual files

--- Vidyant/BAP/Vidyant-Buyer-App-with-Vidyant-Seller-App/GroupA/test.py
+++ Vidyant/BAP/Vidyant-Buyer-App-with-Vidyant-Seller-App/GroupA/test.py
@@ -12,7 +12,7 @@ init = json.load(open('./Flow2/init_11.json'))
 on_init = json.load(open('./Flow2/on_init_12.json'))
 confirm = json.load(open('./Flow2/confirm_13.json'))
 on_confirm = json.load(open('./Flow2/on_confirm_14.json'))
-on_status_pending = json.load(open('./Flow2/on_status_16(solicited).json'))
+on_status_pending = json.load(open('./Flow2/on_status_17(unsolicited)_order_packed.json'))
 on_status_picked = json.load(open('./Flow2/on_status_18(unsolicited)_out_for_delivery.json'))
 on_status_delivered = json.load(open('./Flow2/on_status_18(unsolicited)_out_for_delivery.json'))
 
@@ -22,19 +22,19 @@ payload = json.dumps({
   "domain": "ONDC:RET12",
   "version": "1.2.0",
   "payload": {
-    "search_full_catalog_refresh": {},
+    "search_full_catalog_refresh": search_full_catalog_refresh,
     "on_search_full_catalog_refresh": on_search_full_catalog_refresh,
-    "search_inc_refresh": {},
-    "on_search_inc_refresh": {},
+    "search_inc_refresh": search_inc_refresh,
+    "on_search_inc_refresh": on_search_inc_refresh,
     "select": select,
-    "on_select": {},
-    "init": {},
-    "on_init": {},
-    "confirm": {},
-    "on_confirm": {},
-    "on_status_pending": {},
-    "on_status_picked": {},
-    "on_status_delivered": {}
+    "on_select": on_select,
+    "init": init,
+    "on_init": on_init,
+    "confirm": confirm,
+    "on_confirm": on_confirm,
+    "on_status_pending": on_status_pending,
+    "on_status_picked": on_status_picked,
+    "on_status_delivered": on_status_delivered
   }
 })
 headers = {
