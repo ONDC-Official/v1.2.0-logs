55191c980308cebe9d0816743f378b5d97c07373
kalaivanansundaram <59359484+kalaivanansundaram@users.noreply.github.com>
2024-12-13 12:03:18 +0530

    RSF logs for the mappls buyer app

---
+++ Mappls-Buyer-App/RSF 2.0/flow4/report.php
@@ -0,0 +1,27 @@
+{
+  "context": {
+    "domain": "ONDC:NTS10",
+    "location": {
+      "country": {
+        "code": "IND"
+      },
+      "city": {
+        "code": "*"
+      }
+    },
+    "version": "2.0.0",
+    "action": "report",
+    "bap_id": "mappls.com",
+    "bap_uri": "https://mappls.com/store/ondc-preprod",
+    "bpp_id": "rsf-mock-service.ondc.org/seller_protocol_server_preprod",
+    "bpp_uri": "https://rsf-mock-service.ondc.org/seller_protocol_server_preprod/ondc/",
+    "transaction_id": "0fbcea00a9e9d84c542e_T1732615367",
+    "message_id": "95935f96bee7a55755f9_T1732615367",
+    "timestamp": "2024-11-26T10:09:30Z",
+    "ttl": "P1D"
+  },
+  "message": {
+    "ref_transaction_id": "0fbcea00a9e9d84c542e_T1732615367",
+    "ref_message_id": "95935f96bee7a55755f9_T1732615367"
+  }
+}
